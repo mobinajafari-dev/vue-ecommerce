0cfe87095931ef47ab96a3f800ed51a80366c881
mobina jafari <mobinajafari.dev@gmail.com>
2023-08-19 20:35:00 +0430

    otp added

---
+++ src/components/otp.vue
@@ -0,0 +1,59 @@
+<template>
+  <input
+    class="otp"
+    maxlength="6"
+    placeholder="******"
+    v-model="code"
+    required
+  />
+  <p class="otp__alert">{{ alert[index] }}</p>
+</template>
+
+<script>
+export default {
+  name: "otp",
+  data() {
+    return {
+      code: "",
+      index: 0,
+      alert: [
+        "لطفا کد ارسال شده را وارد نمایید",
+        "کد ارسال شده را به صورت عدد وارد نمایید",
+        "لطفا عدد ارسال شده را به صورت کامل وارد نمایید",
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss">
+$char-w: 1ch;
+$gap: 0.5 * $char-w;
+$n-char: 6;
+$in-w: $n-char * ($char-w + $gap);
+
+.otp {
+  display: block;
+  margin: 1rem auto;
+  border: none;
+  padding: 0;
+  width: $in-w;
+  direction: ltr;
+  background: repeating-linear-gradient(
+      90deg,
+      dimgrey 0,
+      dimgrey $char-w,
+      transparent 0,
+      transparent $char-w + $gap
+    )
+    0 100% / #{$in-w - $gap} 2px no-repeat;
+  font: 5ch droid sans mono, consolas, monospace;
+  letter-spacing: $gap;
+
+  &:focus {
+    outline: none;
+    color: #555;
+  }
+}
+</style>
--- src/views/ConfirmPassword.vue
+++ src/views/ConfirmPassword.vue
@@ -6,36 +6,39 @@
       alt="confirm-password"
       class="user__img"
     />
-    <div class="user__content">
+    <div class="user__content m-auto">
       <img src="@/assets/logo.webp" alt="logo" class="user__logo" />
       <h2 class="user__title">کد دریافتی را وارد نمایید</h2>
       <form class="form m-auto">
-        <input type="text" name="code" id="code" maxlength="6" />
+        <otp />
         <p class="user__alert">تا دریافت کد بعدی</p>
       </form>
       <button class="user__btn single-btn m-auto">ثبت نهایی</button>
-      <div class="password__link mt-1">
-        <p>کد فعال سازی را دریافت نکرده اید؟</p>
-        <router-link :to="{ name: 'forget-password' }" class="pr user__link"
-          >دوباره امتحان کنید</router-link
+      <div class="password__link mt-1 double-btn">
+        <p @click="back" class="pr user__link">→ بازگشت</p>
+        <router-link :to="{ name: 'home' }" class="user__link m-auto pr normal"
+          >بازگشت به خانه ←</router-link
         >
+        <router-view />
       </div>
-      <br />
-      <router-link :to="{ name: 'home' }" class="user__link m-auto"
-        >بازگشت به خانه ←</router-link
-      >
-      <router-view />
     </div>
   </div>
 </template>
 
 <script>
 import NavbarSection from "@/components/NavbarSection.vue";
+import otp from "@/components/otp.vue";
 
 export default {
   name: "ConfirmPassword",
   components: {
     NavbarSection,
+    otp,
+  },
+  methods: {
+    back() {
+      this.$router.go(-1);
+    },
   },
 };
 </script>
